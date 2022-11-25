225e3bb3e1c1fb106692e1a332f9104430ad8479
Jaakko Maekaeraeinen <jaakko.maekaeraeinen@edfenergy.com>
2022-11-25 18:07:35 +0000

    simple single column layout

---
+++ src/components/LatestVideoEmbedded.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const LatestVideoEmbedded: React.FC = () => {
+  return (
+    <div>
+      <h2>My Latest Video :)</h2>
+      <iframe className="embedded-vid" width="420" height="315" title="my-video"
+        src="https://www.youtube.com/embed/qxeQF1Z_PGQ">
+      </iframe>
+    </div>
+  );
+};
+
+export default LatestVideoEmbedded;
