391879e989f8bc0c2f1b0653f4094393d46a1a20
jaakkomaekaeraeinen-edf <76061604+jaakkomaekaeraeinen-edf@users.noreply.github.com>
2022-11-25 20:29:00 +0000

    feat(3): add social media logos (#3)
    * add social medial logos wip
    
    * add socials
    
    Co-authored-by: Jaakko Makarainen <struudeli1@gmail.com>

---
+++ src/styles/styles.ts
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+export const VerticalFlexBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const HorizontalFlexBox = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+`;
