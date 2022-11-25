0b9781853f0a59037c3df93274724305d32f50c3
Jaakko Mäkäräinen <96392583+jacobblackfly@users.noreply.github.com>
2022-11-25 21:15:02 +0000

    feat(4): add background image (#4)

--- src/components/LatestVideoEmbedded.tsx
+++ src/components/LatestVideoEmbedded.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const LatestVideoEmbedded: React.FC = () => {
   return (
     <div>
-      <h2>My Latest Video :)</h2>
+      <h2><span>My Latest Video :)</span></h2>
       <iframe className="embedded-vid" width="420" height="315" title="my-video"
         src="https://www.youtube.com/embed/qxeQF1Z_PGQ">
       </iframe>
