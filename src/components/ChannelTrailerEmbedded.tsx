137e42d969ec05634f8053a6f04078b6559e5b68
Jaakko Mäkäräinen <96392583+jacobblackfly@users.noreply.github.com>
2022-11-27 18:24:16 +0000

    feat(5): update text (#5)
    * add background image
    
    * add coming soomn text

--- src/components/ChannelTrailerEmbedded.tsx
+++ src/components/ChannelTrailerEmbedded.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const ChannelTrailerEmbedded: React.FC = () => {
   return (
     <div>
-      <h2><span>My Channel Trailer :)</span></h2>
+      <h2><span>Another track :)</span></h2>
       <iframe className="embedded-vid" width="420" height="315" title="my-video"
         src="https://www.youtube.com/embed/6c5ePXkhQAk">
       </iframe>
