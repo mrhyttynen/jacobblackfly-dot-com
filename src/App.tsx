a0bf4a6c78d73aa9e28e9e824d6b073aa532d0bb
Jaakko Makarainen <struudeli1@gmail.com>
2025-02-10 09:34:41 +0200

    update background and add links for new single

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import SocialMediaLinks from './components/SocialMediaLinks';
 import { DivWithWhiteBackgroundAndBlackBordersThin, H1WithWhiteBackgroundAndBlackBordrers, VerticalFlexBox } from './styles/styles';
-import MusicLinks from './components/MusicLinks';
+import { SearchingLinks, CarrotCakeLinks } from './components/MusicLinks';
 
 function App() {
   return (
@@ -10,8 +10,12 @@ function App() {
       <body>
         <VerticalFlexBox>
           <H1WithWhiteBackgroundAndBlackBordrers>🎵 JACOBBLACKFLY.COM 🎵</H1WithWhiteBackgroundAndBlackBordrers>
-          <DivWithWhiteBackgroundAndBlackBordersThin>Listen to my new rap about carrot cake below:</DivWithWhiteBackgroundAndBlackBordersThin>
-          <MusicLinks />
+          <DivWithWhiteBackgroundAndBlackBordersThin>Listen to my new song "Searching":</DivWithWhiteBackgroundAndBlackBordersThin>
+          {/* add pic */}
+          <SearchingLinks />
+          <DivWithWhiteBackgroundAndBlackBordersThin>Listen to my rap about carrot cake below:</DivWithWhiteBackgroundAndBlackBordersThin>
+          {/* add pic */}
+          <CarrotCakeLinks />
           <DivWithWhiteBackgroundAndBlackBordersThin>Social media thingies</DivWithWhiteBackgroundAndBlackBordersThin>
           <SocialMediaLinks />
           <DivWithWhiteBackgroundAndBlackBordersThin>Thanks for visiting my page and have a great day! :)</DivWithWhiteBackgroundAndBlackBordersThin>
--- src/components/MusicLinks.tsx
+++ src/components/MusicLinks.tsx
@@ -1,17 +1,27 @@
 import React from 'react';
-import { DivWithWhiteBackgroundAndBlackBordersThick, VerticalFlexBox } from '../styles/styles';
+import { DivWithWhiteBackgroundAndBlackBordersThick, HorizontalFlexBox } from '../styles/styles';
 
-const SocialMediaLinks: React.FC = () => {
+export const CarrotCakeLinks: React.FC = () => {
   return (
     <DivWithWhiteBackgroundAndBlackBordersThick>
-      <VerticalFlexBox>
+      <HorizontalFlexBox>
         <a id="youtube" className="zoom" href="https://www.youtube.com/watch?v=triJ-gSDO38">carrotcake_youtube</a>
         <a id="spotify" className="zoom" href="https://open.spotify.com/track/0zbRd7eFUagrxQfX0BSN5p?si=VJw55mIESuCkVnjaNztklA">carrotcake_spotify</a>
         <a id="apple_music" className="zoom" href="https://music.apple.com/gb/album/180-degrees-carrot-cake-song/1695665758?i=1695665759">carrotcake_apple</a>
         <a id="amazon_music" className="zoom" href="https://music.amazon.co.uk/albums/B0CB2JNF88?marketplaceId=A1F83G8C2ARO7P&musicTerritory=GB&ref=dm_sh_2RkvvEIzWAsR5dySabqS0yCFd&trackAsin=B0CB2SF4C2">carrotcake_amazon</a>
-      </VerticalFlexBox>
+      </HorizontalFlexBox>
     </DivWithWhiteBackgroundAndBlackBordersThick>
   );
 };
 
-export default SocialMediaLinks;
+export const SearchingLinks: React.FC = () => {
+  return (
+    <DivWithWhiteBackgroundAndBlackBordersThick>
+      <HorizontalFlexBox>
+        <a id="spotify" className="zoom" href="https://open.spotify.com/album/2hqZceh2GVkHgqy5mt2Rbg">searching_spotify</a>
+        <a id="apple_music" className="zoom" href="https://music.apple.com/us/album/searching/1794303799">searching_apple</a>
+        <a id="amazon_music" className="zoom" href="https://www.amazon.com/music/player/tracks/B0DVQ54M2L">searching_amazon</a>
+      </HorizontalFlexBox>
+    </DivWithWhiteBackgroundAndBlackBordersThick>
+  );
+};
