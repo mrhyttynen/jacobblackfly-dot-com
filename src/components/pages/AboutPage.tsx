5f9734fe04b1448d6830f699a0919caac91a9acb
Jaakko Makarainen <makarainenjaakko@gmail.com>
2025-08-24 16:21:39 +0300

    update bio 8/2025

--- src/components/pages/AboutPage.tsx
+++ src/components/pages/AboutPage.tsx
@@ -4,39 +4,19 @@ export const AboutPage: React.FC = () => {
   return (
     <DivWithWhiteBackgroundAndBlackBordersThin>
 Few artists can claim that their debut single is about cake. Jacob Blackfly can, though that doesn’t mean 
-desserts are the only thing he talks about in his self-produced indie-pop songs. 
-Originally from Brussels and currently living in Helsinki, Jacob Blackfly grew up surrounded by a mix of 
-2000s and 2010s pop, classical music and classic rock and blues, before later discovering electronic 
-music and RnB. These genres have shaped Jacob Blackfly’s sound to range from piano ballads infused 
-with electronic elements, through pop-rock to 90s-style hip hop, but with a consistent focus on catchy 
-melodies and memorable grooves. 
-<br></br>
-<br></br>
-With influences ranging from Adele and Corinne Bailey Rae through AC/DC and The Beatles to Rufus Du Sol 
-and Calvin Harris, Jacob’s simple and to-the-point lyrics about topics such as relationships, food and 
-man’s search for meaning are underpinned by a playful approach to life and combine with the unusual harmony 
-of his tracks to form a catchy yet intriguing listening experience.
-<br></br>
-<br></br>
-Encouraged from a young age by his parents to pursue a musical hobby, Jacob undertook eight years of 
-classical piano and music theory training, which enabled him to start writing his own music upon reaching 
-university to study Economics. Performances of classical works as member of various choirs including the 
-<a href="https://bfc.org.uk/" target="_blank" className="linkinsidetext"> Brighton Festival Chorus</a> and 
-<a href="https://solkuoro.fi/" target="_blank" className="linkinsidetext"> Savolaisen Osakunnan Laulajat </a>
-developed Jacob as a singer and performer. After working as a software engineer for 3 years 
-Jacob quit his job to be able to devote more time to music, getting accepted to the music 
-program at Laajasalon Opisto.
-<br></br>
-<br></br>
-Jacob’s most recent projects include 2 single releases, “180 Degrees” and “Searching”, the former reaching 
-1000 streams on Youtube. Currently Jacob is in the studio working on his debut EP, due out in June 2025, 
-which will feature an additional 3 songs. Jacob is also co-writing and producing a single “Leikitään” 
-with Kultapoiki, due to come out late 2025. Lastly, Jacob is actively gigging in Helsinki performing 
-his own music (most recently at four Aitoklubi events at Ylistalo and at Playhouse Bar) and as a band 
-member in other musicians’ projects, including being part of the house band for a kaupunki.tv livestream 
-in November 2024. Kallio Block Party and Herttoniemi Block Party founder, member of Kontula Electronic 
-working group Mikko Heinänen comments on Jacob’s music (Feb 2025): “Jacob has a clearly distinctive 
-voice in his songs.”
+desserts are the only thing he talks about in his self-produced indie-pop songs. Born and bred in Brussels 
+with Finnish roots and currently living in Helsinki, Jacob Blackfly grew up surrounded by a mix of vintage 
+and contemporary pop, classical music and classic rock and blues, before later discovering electronic music 
+and RnB (Adele, Corinne Bailey Rae, AC/DC, The Beatles, Rufus Du Sol, Moonchild). These genres have each 
+left their mark on Jacob’s sound, which includes piano ballads with an electronic touch, pop-rock and even 
+90s-style hip hop, keeping a consistent focus on catchy melodies and memorable grooves. June 2025 saw the 
+release of Jacob’s 4-track debut EP “Searching”, which includes the singles “Avoidant”, “Searching” and “180 Degrees”, 
+as well as music videos for the latter 2. The EP has garnered 2000+ streams across platforms. Jacob is also 
+co-writing and producing a single “Leikitään” with Kultapoiki, due to come out late 2025. Lastly, Jacob is 
+actively gigging in Helsinki to promote his music (most recently at Käpygrilli and Linna Bar in June 2025) 
+and as a band member in other musicians’ projects. Kallio Block Party and Herttoniemi Block Party founder, 
+member of Kontula Electronic working group Mikko Heinänen comments on Jacob’s music (Feb 2025): “Jacob has 
+a clearly distinctive voice in his songs.”
     </DivWithWhiteBackgroundAndBlackBordersThin>
   );
 };
