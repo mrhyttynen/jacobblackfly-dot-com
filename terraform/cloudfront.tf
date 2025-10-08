4099ae41fd366bece3327a64a6713ccb3d0c667a
Jaakko Makarainen <makarainenjaakko@gmail.com>
2025-10-08 18:27:13 +0300

    migrate cloudfront distributions to tf

--- terraform/cloudfront.tf
+++ terraform/cloudfront.tf
@@ -86,3 +86,83 @@ resource "aws_cloudfront_distribution" "www_distribution" {
     ssl_support_method             = "sni-only"
   }
 }
+
+resource "aws_cloudfront_distribution" "root_distribution" {
+  aliases                         = ["jacobblackfly.com"]
+  anycast_ip_list_id              = null
+  comment                         = "jacobblackfly.com ROOT domain distribution"
+  continuous_deployment_policy_id = null
+  default_root_object             = null
+  enabled                         = true
+  http_version                    = "http2"
+  is_ipv6_enabled                 = true
+  price_class                     = "PriceClass_All"
+  retain_on_delete                = false
+  staging                         = false
+  tags = {
+    Name = "jacobblackfly.com"
+  }
+  tags_all = {
+    Name = "jacobblackfly.com"
+  }
+  wait_for_deployment = true
+  web_acl_id          = null
+  custom_error_response {
+    error_caching_min_ttl = 10
+    error_code            = 403
+    response_code         = 200
+    response_page_path    = "/index.html"
+  }
+  default_cache_behavior {
+    allowed_methods            = ["GET", "HEAD"]
+    cache_policy_id            = "4135ea2d-6df8-44a3-9df3-4b5a84be39ad"
+    cached_methods             = ["GET", "HEAD"]
+    compress                   = true
+    default_ttl                = 0
+    field_level_encryption_id  = null
+    max_ttl                    = 0
+    min_ttl                    = 0
+    origin_request_policy_id   = null
+    realtime_log_config_arn    = null
+    response_headers_policy_id = null
+    smooth_streaming           = false
+    target_origin_id           = "jacobblackfly.com.s3-website.eu-west-2.amazonaws.com"
+    trusted_key_groups         = []
+    trusted_signers            = []
+    viewer_protocol_policy     = "allow-all"
+    grpc_config {
+      enabled = false
+    }
+  }
+  origin {
+    connection_attempts         = 3
+    connection_timeout          = 10
+    domain_name                 = "jacobblackfly.com.s3-website.eu-west-2.amazonaws.com"
+    origin_access_control_id    = null
+    origin_id                   = "jacobblackfly.com.s3-website.eu-west-2.amazonaws.com"
+    origin_path                 = null
+    response_completion_timeout = 0
+    custom_origin_config {
+      http_port                = 80
+      https_port               = 443
+      ip_address_type          = null
+      origin_keepalive_timeout = 5
+      origin_protocol_policy   = "http-only"
+      origin_read_timeout      = 30
+      origin_ssl_protocols     = ["SSLv3", "TLSv1", "TLSv1.1", "TLSv1.2"]
+    }
+  }
+  restrictions {
+    geo_restriction {
+      locations        = []
+      restriction_type = "none"
+    }
+  }
+  viewer_certificate {
+    acm_certificate_arn            = "arn:aws:acm:us-east-1:304315860042:certificate/eff1ec19-f3e8-40bf-9c6d-c1e1599b6c01"
+    cloudfront_default_certificate = false
+    iam_certificate_id             = null
+    minimum_protocol_version       = "TLSv1.2_2021"
+    ssl_support_method             = "sni-only"
+  }
+}
